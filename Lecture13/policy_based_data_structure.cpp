ef312ec2d2b5f91c272e72db9be1386a410e0635
baazigar31 <imanand731@gmail.com>
2021-10-23 04:35:57 +0530

    Policy Based Data Structure

--- Lecture13/policy_based_data_structure.cpp
+++ Lecture13/policy_based_data_structure.cpp
@@ -1,11 +1,11 @@
 // Problem Link - 
 /* By Shubham Anand */
 #include<bits/stdc++.h>
-//#include<ext/pb_ds/assoc_container.hpp>
-//#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 //#include <ext/pb_ds/trie_policy.hpp>
-//using namespace __gnu_pbds;
-using namespace std;
+using namespace __gnu_pbds;
+// using namespace std;
 #define ll 				long long int
 #define ld				long double
 #define mod             1000000007
@@ -13,13 +13,13 @@ using namespace std;
 #define endl			"\n"
 #define pb 				push_back
 #define vi              vector<ll>
-#define vs				vector<string>
+#define vs				std::vector<std::string>
 #define pii             pair<ll,ll>
 #define ump				unordered_map
 #define mp 				make_pair
 #define pq_max          priority_queue<ll>
 #define pq_min          priority_queue<ll,vi,greater<ll> >
-// #define all(n) 			n.begin(),n.end()
+#define all(n) 			n.begin(),n.end()
 #define ff 				first
 #define ss 				second
 #define mid(l,r)        (l+(r-l)/2)
@@ -27,71 +27,47 @@ using namespace std;
 #define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
 #define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
 #define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
-#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define log(args...) 	{ std::string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
 #define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
 template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
 template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
-vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
+vs tokenizer(std::string str,char ch) {std::istringstream var((str)); vs v; std::string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
 
 
-void err(istream_iterator<string> it) {}
+void err(std::istream_iterator<std::string> it) {}
 template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cout << *it << " = " << a << endl;
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+	std::cout << *it << " = " << a << endl;
 	err(++it, args...);
 }
-//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef tree<ll, null_type, std::less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 //typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
 
 void file_i_o()
 {
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
+    std::ios_base::sync_with_stdio(0); 
+    std::cin.tie(0); 
+    std::cout.tie(0);
 	#ifndef ONLINE_JUDGE
 	    freopen("input.txt", "r", stdin);
 	    freopen("output.txt", "w", stdout);
 	#endif
 }
 
-
 int main(int argc, char const *argv[]) {
 	clock_t begin = clock();
 	file_i_o();
 	// Write your code here....
 
-	//Bitset
+	pbds p;
+	p.insert(6);
+	p.insert(8);
+	p.insert(10);
+	p.insert(12);
+	p.insert(14);
 
-	bitset<16> b1;
-	// b1="8";
-	bitset<16> b2(2);
-	bitset<16> b3("0100");
-	log(b1,b2,b3);
-	b3[2]=0;
-	log(b3);
-
-
-	cout<<b3.count()<<endl; //Returns no. of setbits
-	cout<<b3.any()<<endl; 
-	cout<<b2.any()<<endl;
-	b2[1]=0;
-	cout<<b2.any()<<endl; // returns true if any of them is set
-	cout<<b2.all()<<endl; // Returna 1 if all set
-
-	b3.set(); //Sets all the bit
-	log(b3);
-	cout<<b3.all()<<endl;
-
-	b3.reset();
-	log(b3);
-	cout<<b3.all()<<endl;
-
-	b3.flip();
-	log(b3);
-
-
-
-
+	std::cout<<*p.find_by_order(1)<<endl;
+	std::cout<<p.order_of_key(11)<<endl;
 
 
 
